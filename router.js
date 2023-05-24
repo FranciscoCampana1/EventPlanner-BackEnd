76d55bc4f5be9fd5d25d1af0120728d1d2aa78ef
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-24 17:38:11 +0200

    feat: create routes for users and events

--- router.js
+++ router.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const indexRouter = require('./routes/index')
-const authRouter = require('./routes/auth')
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users')
+const eventRouter = require('./routes/event')
 
 //Ruta para index
 router.use('/api', indexRouter)
 
 router.use('/api/auth', authRouter)
+
+router.use('/api/users', userRouter)
+
+router.use('/api/event', eventRouter)
+
 
 
 module.exports = router;
