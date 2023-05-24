61f4c9e79b44c6e9bcbba589fdcf0009c3228db1
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-24 15:57:17 +0200

    chore: add 'api' in the routes

--- router.js
+++ router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const indexRouter = require('./routes/index')
+const authRouter = require('./routes/auth')
 
 //Ruta para index
-router.use('/', indexRouter)
+router.use('/api', indexRouter)
 
 router.use('/api/auth', authRouter)
 
