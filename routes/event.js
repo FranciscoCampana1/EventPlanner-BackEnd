34932288c443822f5322db91a273983bd68a8eb0
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-25 18:43:58 +0200

    feat: created routes for events

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,15 @@
-var express = require('express');
-const eventController = require('../controllers/eventController');
-const verifyToken = require('../middleware/verifyToken');
+var express = require("express");
+const eventController = require("../controllers/eventController");
+const verifyToken = require("../middleware/verifyToken");
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/create', verifyToken ,eventController.createEvent)
+router.post("/create", verifyToken, eventController.createEvent);
+
+router.get("/get-events", verifyToken, eventController.getById);
+
+router.put("/update-event/:id", verifyToken, eventController.updateEvent);
+
+router.delete('/delete-event', verifyToken, eventController.deleteEvent)
 
 module.exports = router;
