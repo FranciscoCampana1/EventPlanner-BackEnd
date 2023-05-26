049bf17d5f1a65c3232e463880b3743ce1d992b8
Francisco Campana <campanafrancisco1@gmail.com>
2023-05-26 15:39:11 +0200

    chore : change leguage to english

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -3,7 +3,7 @@ const { sendErrorResponse } = require("../_util/sendResponse");
 const isAdmin = (req, res, next) => {
   const { user_role } = req;
   if (user_role != "admin") {
-    return sendErrorResponse(res, 403, "No tiene los permisos necesarios");
+    return sendErrorResponse(res, 403, "you don't have the necessary permissions");
   } else next();
 };
 
--- middleware/isUser.js
+++ middleware/isUser.js
@@ -5,7 +5,7 @@ const isUser = async (req, res, next) => {
   const {user_role} = req;
 
   if(user_role != "user"){
-    return sendErrorResponse (res, 403, "no tiene los permisos necesarios")
+    return sendErrorResponse (res, 403, "you don't have the necessary permissions")
   }else next()
 };
 module.exports = isUser;
